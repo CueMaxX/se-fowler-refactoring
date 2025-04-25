211f9351b0b33fc633ecfd0bee21846a57cd54fa
Ephraim Böhlert <info@cmxp.de>
2025-04-25 16:53:19 +0200

    encapsulation

--- src/Movie.java
+++ src/Movie.java
@@ -6,14 +6,24 @@ public class Movie {
     private int priceCode;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(priceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return _price.getPriceCode(); 
     }
-    public void setPriceCode(int arg) {
-        priceCode = arg;
+    public void setPriceCode(int arg) { 
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice(); break;
+            case CHILDRENS:
+                _price = new ChildrensPrice(); break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice(); break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        } 
     }
+private Price _price;
     public String getTitle (){
         return title;
     };
