e62638bb95558be85a3d13b4e8803f9aaebd8b5c
Ephraim Böhlert <info@cmxp.de>
2025-04-25 16:41:37 +0200

    convert to html

--- src/Customer.java
+++ src/Customer.java
@@ -14,18 +14,18 @@ class Customer {
     public String getName (){
         return name;
     };
-    public String statement() {
+    public String htmlStatement() {
         Enumeration enum_rentals = rentals.elements();	    
-        String result = "Rental Record for " + this.getName() + "\n";
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalfrequentRenterPoints()) + " frequent renter points";
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + String.valueOf(getTotalfrequentRenterPoints()) + "</EM> frequent renter points<P>";
         return result;
     }
     private int getTotalfrequentRenterPoints() {
